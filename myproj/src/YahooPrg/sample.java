e315c6eb53bf5dfce73ddc1719a2041c9b286288
venkat <venkat@venkat-PC>
2019-01-27 10:54:45 +0530

    updated sample program

--- myproj/src/YahooPrg/sample.java
+++ myproj/src/YahooPrg/sample.java
@@ -1,8 +1,14 @@
 package YahooPrg;
 
-public class sample {
-public void f1()
+public class sample 
 {
-}
+	public void f1()
+	{
+		System.out.println("hello");
+	}
+	public void f2()
+	{
+		System.out.println("this is for testing");
+	}
 }
 
