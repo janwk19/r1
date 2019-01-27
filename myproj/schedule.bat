d9ce3882fabca2755ee3ec768603b160b449e6b0
venkat <venkat@venkat-PC>
2019-01-27 11:26:42 +0530

    updated

--- myproj/schedule.bat
+++ myproj/schedule.bat
@@ -1,4 +1,4 @@
-set classpath=d:\janwk05\myproj\bin;d:\sel_dec18\Library\*;
+set classpath=D:\janwk05\mylocalrep\myproj\bin;d:\sel_dec18\Library\*;
 
 java org.testng.TestNG D:\janwk05\mylocalrep\myproj\src\yahoo_testing.xml
 
